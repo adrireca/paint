05356825e6099c64ea89e499a50cee6dcd856287
Adrian <reyes3190@gmail.com>
2023-10-17 21:15:35 +0200

    cambios estilos y lógica

--- pintarrajea.js
+++ pintarrajea.js
@@ -5,18 +5,20 @@ let seleccionado = document.querySelector(".seleccionado");
 let pintar = false;
 let color;
 
-//genero una tabla al cargarse la página.
+//Genera una tabla al cargarse la página.
 function generarTabla() {
   let tabla = document.createElement("table");
+  tabla.setAttribute('class', 'tabla');
+
   let tblBody = document.createElement("tbody");
 
-  for (var i = 0; i < 60; i++) {
+  for (var i = 0; i < 30; i++) {
     let tr = document.createElement("tr");
     for (var j = 0; j < 50; j++) {
       let td = document.createElement("td");
-      td.setAttribute('name','celdas');
-      td.setAttribute('onclick','esPintar()');
-      td.setAttribute('onmouseover','pintarCelda(this)');
+      td.setAttribute('name', 'celdas');
+      td.setAttribute('onclick', 'esPintar()');
+      td.setAttribute('onmouseover', 'pintarCelda(this)');
       tr.appendChild(td);
     }
     tblBody.appendChild(tr);
@@ -30,57 +32,96 @@ function generarTabla() {
 let selector = document.getElementById("selector");
 selector.onclick = seleccionarColor;
 
-//selecciono el color a pintar.
-function seleccionarColor(e){
+let selectorBorrar = document.querySelector('.borrar');
+selectorBorrar.onclick = seleccionarColorBorrar;
+
+
+function seleccionarColorBorrar(e) {
+  console.log(e.target.classList[0]);
+  color = e.target.classList[0];
+
+  if (color === 'blanco') {
+    selectorBorrar.setAttribute('id', `celda${color}`);
+  }
+}
+
+let rojo = document.querySelector('.rojo');
+let amarillo = document.querySelector('.amarillo');
+let verde = document.querySelector('.verde');
+let azul = document.querySelector('.azul');
+let lila = document.querySelector('.lila');
+let rosa = document.querySelector('.rosa');
+
+//selecciona el color a pintar.
+function seleccionarColor(e) {
   color = e.target.classList.value;
-  //además informo al usuario el color a pintar.
-  if(color === 'rojo'){
-    selector.setAttribute('class',`celda${color}`);
-    seleccionado.innerHTML = 'SELECCIONADO: rojo';
-  }else if(color === 'amarillo'){
-    selector.setAttribute('class',`celda${color}`);
-    seleccionado.innerHTML = 'SELECCIONADO: amarillo';
-  }else if(color === 'verde'){
-    selector.setAttribute('class',`celda${color}`);
-    seleccionado.innerHTML = 'SELECCIONADO: verde';
-  }else if(color === 'azul'){
-    selector.setAttribute('class',`celda${color}`);
-    seleccionado.innerHTML = 'SELECCIONADO: azul';
-  }else if(color === 'lila'){
-    selector.setAttribute('class',`celda${color}`);
-    seleccionado.innerHTML = 'SELECCIONADO: lila';
-  }else if(color === 'blanco'){
-    selector.setAttribute('class',`celda${color}`);
-    seleccionado.innerHTML = 'SELECCIONADO: blanco';
+
+
+
+  //Informa al usuario el nombre del color a pintar.
+  if (color === 'rojo') {
+    selector.setAttribute('class', `celda${color}`);
+    reset();
+    rojo.innerHTML = 'Rojo';
+  } else if (color === 'amarillo') {
+    selector.setAttribute('class', `celda${color}`);
+    reset();
+    amarillo.innerHTML = 'Amarillo';
+  } else if (color === 'verde') {
+    selector.setAttribute('class', `celda${color}`);
+    reset();
+    verde.innerHTML = 'Verde';
+  } else if (color === 'azul') {
+    selector.setAttribute('class', `celda${color}`);
+    reset();
+    azul.innerHTML = 'Azul';
+  } else if (color === 'lila') {
+    selector.setAttribute('class', `celda${color}`);
+    reset();
+    lila.innerHTML = 'Lila';
+  } else if (color === 'rosa') {
+    selector.setAttribute('class', `celda${color}`);
+    reset();
+    rosa.innerHTML = 'Rosa';
   }
-  
+
 }
 
-//añado la clase css para el color elegido.
-function pintarCelda(celda){
-  if(pintar){
-    celda.setAttribute('class',`celda${color}`);
+//Añade la clase css para el color elegido.
+function pintarCelda(celda) {
+  if (pintar) {
+    celda.setAttribute('class', `celda${color}`);
   }
 }
 
-//inicio o detengo pintar, según el click.
-function esPintar(){
-  if(pintar){
+//Inicia o detiene pintar, según el click.
+function esPintar() {
+  if (pintar) {
     pintar = false;
-  }else{
+  } else {
     pintar = true;
   }
 }
 
-//limpio el lienzo.
-let boton = document.querySelector('.btn');
+//Resetea el lienzo.
+let boton = document.querySelector('.reset');
 boton.addEventListener('click', () => {
   let celdas = document.getElementsByName('celdas');
-  for(let i=0; i<celdas.length; i++){
+  for (let i = 0; i < celdas.length; i++) {
     celdas[i].removeAttribute('class');
   }
+  reset();
 });
 
 
-//al cargar la página llamo a la función.
+function reset() {
+  rojo.innerHTML = '';
+  amarillo.innerHTML = '';
+  verde.innerHTML = '';
+  azul.innerHTML = '';
+  lila.innerHTML = '';
+  rosa.innerHTML = '';
+}
+
+//al cargar la página llama a la función.
 window.addEventListener("load", generarTabla, false);
